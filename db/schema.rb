21ec771e02fe80df0c3cea3c0608f82c72b9f92b
ssenabulyawilsonm@gmail.com <ssenabulyawilsonm@gmail.com>
2015-06-16 03:54:28 +0300

    reviews scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150615201506) do
+ActiveRecord::Schema.define(version: 20150616001722) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -26,6 +26,15 @@ ActiveRecord::Schema.define(version: 20150615201506) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "rating"
+    t.text     "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "user_id"
+    t.string   "integer"
   end
 
   create_table "users", force: :cascade do |t|
